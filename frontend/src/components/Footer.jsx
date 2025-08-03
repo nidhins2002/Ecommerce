40a53d27c4a9462f47e5c323ad9090471d111499
nidhins2002 <nidhins2020@gmail.com>
2025-08-04 00:03:30 +0530

    Added search bar and search functionality,product page and related products

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
-                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <img src={assets.logo1} className='mb-5 w-32' alt="" />
                 <p className='w-full md:w-2/3 text-gray-600'>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero nesciunt eos culpa sapiente ipsam laudantium eveniet rem necessitatibus, in cum illo aliquam saepe laborum quam.
                 </p>
